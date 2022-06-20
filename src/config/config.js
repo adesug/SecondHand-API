76f074542a17f19ed7152c32dfc1b6d5bf7de324
dhimweray222 <dhimweray222@gmail.com>
2022-06-20 19:28:34 +0700

    Authorization

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   
     "development": {
       "username": "postgres",
-      "password": "plugin",
+      "password": "dimasslalu123",
       "database": "Secondhand",
       "host": "127.0.0.1",
       "dialect": "postgres"
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const ProductController = require('../controllers/produkControllers')
 const multer = require('multer')
 const storage = require('../helpers/multerStorage.helper')
+const {
+  authentication
+} = require('../helpers/authMiddlewares.helper')
 
 const fs = require('fs');
 const upload = multer({
@@ -20,18 +23,22 @@ const upload = multer({
 })
 
 
+router.post('/create', authentication, upload.fields([{
+  name: 'foto_produk_1',
+  maxCount: 1
+}, {
+  name: 'foto_produk_2',
+  maxCount: 1
+}, {
+  name: 'foto_produk_3',
+  maxCount: 1
+}]), ProductController.create)
 
 
-
-router.post('/create',upload.fields([{
-           name: 'foto_produk_1', maxCount: 1
-         }, {
-           name: 'foto_produk_2', maxCount: 1
-         },{
-           name: 'foto_produk_3', maxCount: 1
-         }]), ProductController.create)
-
-
+router.get('/list', ProductController.list)
+router.get('/list-by-id', authentication, ProductController.listId)
+router.delete('/deleteByUser/:id', authentication, ProductController.deleteByUser)
+router.delete('/delete-after-sold/:id', authentication, ProductController.deleteAfterSold)
 
 
 module.exports = router
