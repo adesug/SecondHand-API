dce364d65db95caeac943e64d3cad2b81e4468b4
adesug <adesugiantoro89@gmail.com>
2022-07-06 20:24:50 +0700

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./src/routes/index')
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(routes)
