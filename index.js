e7c8008f5130c89177b9b71d15e564a365bd291a
dhimweray222 <dhimweray222@gmail.com>
2022-07-12 19:30:00 +0700

    menambahkan swagger

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./src/routes/index')
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger.json')
+
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(routes)
-
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.listen(port,() => {
     console.log(`server running ${port}`)
 })
